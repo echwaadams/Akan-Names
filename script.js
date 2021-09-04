edde874f81e94e7bd65cb34d1acfda7cb8e8c521
echwaadams <adamsechwa18@gmail.com>
2021-09-04 19:47:56 +0300

    add alert to display user akan name

--- script.js
+++ script.js
@@ -9,11 +9,27 @@ function validation () {
     var day = document.getElementById('birthDate').value;
     var gender = document.getElementById('gender').value
 
+    var date = new Date(year + "/" + month + "/" + day);
+    var d = date.getDay();
+
     if((day <= 0) || (day > 31)) {
         alert('Please enter valid dates!')
     }
     else if ((month <= 0) || (month > 12)) {
         alert('Please enter valid months!')
+    }
+    else if ((year <= 1800) || (year > 2099)) {
+        alert("Check the century entered.")
+    }
+    else if(gender === null){
+        alert("Please choose your gender")
+    }
+
+    if(gender === "Female"){
+        alert("Your Akan name is: " + femaleNames[d] + " and you were born on " + dayOfTheWeek[d])
+    }
+    else if (gender === "Male"){
+        alert("Your Akan name is: " + maleNames[d] + " and you were born on " + dayOfTheWeek[d])
     }
 }
 
