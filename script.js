ceefe8eb6bd27d43fd50dbb438d44a93981cd35d
echwaadams <adamsechwa18@gmail.com>
2021-09-04 19:04:59 +0300

    add a function to alert the user when incorrects entered

--- script.js
+++ script.js
@@ -2,3 +2,22 @@
 var dayOfTheWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
 var femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+
+function validation () {
+    var year = document.getElementById('birthYear').value;
+    var month = document.getElementById('birthMonth').value;
+    var day = document.getElementById('birthDate').value;
+    var gender = document.getElementById('gender').value
+
+    if((day <= 0) || (day > 31)) {
+        alert('Please enter valid dates!')
+    }
+    else if ((month <= 0) || (month > 12)) {
+        alert('Please enter valid months!')
+    }
+}
+
+
+    
+
+
